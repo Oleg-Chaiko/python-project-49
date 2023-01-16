f6c61ecf3fc30ce522c87db9512df70b9d35eb31
Oleg <chaiko.oleg89@gmail.com>
2023-01-16 21:06:27 +0300

    fix: prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,17 +1,15 @@
 import random
 
 
-def is_prime(n):
-    d = 2
-    while n % d != 0:
-        d += 1
-    if d == n:
-        return 'yes'
-    else:
-        return 'no'
-
-
 def prime_game():
+    def is_prime(n):
+        d = 2
+        while n % d != 0:
+            d += 1
+        if d == n:
+            return 'yes'
+        else:
+            return 'no'
     random_number = random.randint(1, 100)
     corect_ansver = is_prime(random_number)
     return random_number, corect_ansver
