fd470c66341430819884ef56d814ab852c39c3ad
Oleg <chaiko.oleg89@gmail.com>
2023-01-16 20:33:34 +0300

    engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -28,7 +28,7 @@ Correct answer was '{corect_ansver}'")
             print(f"Let's try again, {name}")
             break
     else:
-        print(f'Congratulations, {name}')
+        print(f'Congratulations, {name}!')
 
 
 def even():
